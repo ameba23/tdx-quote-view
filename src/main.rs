4aec8692e317bc201dc47c85a00e129ff5a35687
peg <ameba23@systemli.org>
2024-10-25 15:51:41 +0200

    Initial commit

---
+++ src/main.rs
@@ -0,0 +1,20 @@
+use configfs_tsm::OpenQuote;
+use tdx_quote::Quote;
+
+fn main() {
+    // If an argument is given it is used as the quote name
+    let quote_name = std::env::args().nth(1).unwrap_or("test-quote".to_string());
+    let mut quote = OpenQuote::new(&quote_name).unwrap();
+
+    // Assert that the provider must be TDX
+    quote.check_provider(vec!["tdx_guest"]).unwrap();
+
+    // Give 64 null bytes as input data
+    quote.write_input([0; 64]).unwrap();
+
+    let output = quote.read_output().unwrap();
+
+    let quote_structured = Quote::from_bytes(&output).unwrap();
+    println!("Quote: {:?}", quote_structured);
+    println!("Generation: {}", quote.read_generation().unwrap());
+}
